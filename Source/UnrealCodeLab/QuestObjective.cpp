c9c27247132f7f1aa97a950e19f2a4787a5e4202
william.tjang <silentwill29@gmail.com>
2021-12-30 01:41:45 +1100

    Working on questobjective class

--- Source/UnrealCodeLab/QuestObjective.cpp
+++ Source/UnrealCodeLab/QuestObjective.cpp
@@ -1,5 +1,17 @@
 #include "QuestObjective.h"
+#include "CustomFunctionLibrary.h"
 #include "Quest.h"
+#include "QuestManager.h"
+
+bool UQuestObjective::IsObjectiveStatusBlocked(const FProgressStatusBlockFlags& Flags)
+{
+	return (ObjectiveStatus == EProgressStatus::Locked && Flags.bBlockLocked) ||
+		(ObjectiveStatus == EProgressStatus::Unlocked && Flags.bBlockUnlocked) ||
+		(ObjectiveStatus == EProgressStatus::Started && Flags.bBlockStarted) ||
+		(ObjectiveStatus == EProgressStatus::Abandoned && Flags.bBlockAbandoned) ||
+		(ObjectiveStatus == EProgressStatus::Failed && Flags.bBlockFailed) ||
+		(ObjectiveStatus == EProgressStatus::Completed && Flags.bBlockCompleted);
+}
 
 void UQuestObjective::SetObjectiveStatus(bool bIsComplete)
 {
@@ -10,4 +22,52 @@ void UQuestObjective::SetObjectiveStatus(bool bIsComplete)
 	}
 
 	bIsCompleted = bIsComplete;
+}
+
+bool UQuestObjective::LockObjective(FProgressStatusBlockFlags Flags)
+{
+	if (Flags.bUseDefaultFlags)
+	{
+		Flags.bBlockLocked = true;
+	}
+
+	if (!IsObjectiveStatusBlocked(Flags))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Lock objective failed: Objective status is blocked."));
+		return false;
+	}
+
+	ObjectiveStatus = EProgressStatus::Locked;
+	OnObjectiveLocked();
+	UCustomFunctionLibrary::GetQuestManager(this)->OnAnyObjectiveLocked.Broadcast(this);
+
+	return true;
+}
+
+bool UQuestObjective::UnlockObjective(FProgressStatusBlockFlags Flags)
+{
+	if (Flags.bUseDefaultFlags)
+	{
+		Flags.bBlockStarted = true;
+		Flags.bBlockAbandoned = true;
+		Flags.bBlockFailed = true;
+		Flags.bBlockCompleted = true;
+	}
+
+	if (!IsObjectiveStatusBlocked(Flags))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Lock objective failed: Objective status is blocked."));
+		return false;
+	}
+
+	ObjectiveStatus = EProgressStatus::Unlocked;
+	OnObjectiveUnlocked();
+	UCustomFunctionLibrary::GetQuestManager(this)->OnAnyObjectiveUnlocked.Broadcast(this);
+
+	return true;
+}
+
+bool UQuestObjective::CheckCompletionConditions_Implementation()
+{
+	return true;
 }
--- Source/UnrealCodeLab/QuestObjective.h
+++ Source/UnrealCodeLab/QuestObjective.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "UObject/NoExportTypes.h"
+#include "PublicObjectTypes.h"
 #include "QuestObjective.generated.h"
 
 class UQuest;
@@ -28,30 +29,74 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	FName ObjectiveID;
 
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Type")
-	EQuestObjectiveType ObjectiveType;
-
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Description")
 	FText ObjectiveDescription;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, DisplayName = "Type")
+	EQuestObjectiveType ObjectiveType;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	bool bIsHidden;
-
+	
 	UPROPERTY(EditAnywhere, BlueprintReadWrite)
 	bool bIsLocked;
-
-	UPROPERTY(EditAnywhere, BlueprintReadWrite)
-	bool bIsRepeatable;
 
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	bool bIsCompleted;
 
+private:
+	EProgressStatus ObjectiveStatus;
+
 public:
+	bool IsObjectiveStatusBlocked(const FProgressStatusBlockFlags& Flags);
+
+	UFUNCTION(BlueprintCallable, Category = "Quest")
+	// Returns true if objective is able to be locked, false otherwise.
+	bool LockObjective(FProgressStatusBlockFlags Flags);
+
+	UFUNCTION(BlueprintCallable, Category = "Quest")
+	// Returns true if quest is able to be unlocked, false otherwise.
+	bool UnlockObjective(FProgressStatusBlockFlags Flags);
+
+	//UFUNCTION(BlueprintCallable, Category = "Quest")
+	//// Returns true if quest is able to be started, false otherwise.
+	//bool StartObjective(FProgressStatusBlockFlags Flags);
+
+	//UFUNCTION(BlueprintCallable, Category = "Quest")
+	//// Returns true if quest is able to be abandoned, false otherwise.
+	//bool AbandonObjective(FProgressStatusBlockFlags Flags);
+
+	//UFUNCTION(BlueprintCallable, Category = "Quest")
+	//// Returns true if quest is able to be failed, false otherwise.
+	//bool FailObjective(FProgressStatusBlockFlags Flags);
+
+	//UFUNCTION(BlueprintCallable, Category = "Quest")
+	//// Returns true if quest is able to be completed, false otherwise.
+	//bool CompleteObjective(FProgressStatusBlockFlags Flags);
+
 	UFUNCTION(BlueprintCallable)
 	void SetObjectiveStatus(bool bIsComplete);
 
-	UFUNCTION(BlueprintImplementableEvent)
+	UFUNCTION(BlueprintNativeEvent)
 	bool CheckCompletionConditions();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveLocked();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveUnlocked();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveStarted();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveAbandoned();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveFailed();
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void OnObjectiveCompleted();
 
 	UFUNCTION(BlueprintImplementableEvent)
 	void ResetProgress();
