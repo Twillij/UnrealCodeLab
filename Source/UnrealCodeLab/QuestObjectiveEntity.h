3db9450e8a59be8242bfee3b1fbf66e6b2c2b62e
william.tjang <silentwill29@gmail.com>
2022-01-01 18:55:55 +1100

    Renamed quest interface

---
+++ Source/UnrealCodeLab/QuestObjectiveEntity.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "QuestInterface.generated.h"
+
+class UQuestManager;
+
+UINTERFACE(MinimalAPI, BlueprintType)
+class UQuestObjectiveEntity : public UInterface
+{
+	GENERATED_BODY()
+};
+
+class UNREALCODELAB_API IQuestObjectiveEntity
+{
+	GENERATED_BODY()
+
+public:
+
+};
